bb225949d889076ec8dd86a2c6fbdc199702570e
39 MK <miku-fans@hotmail.com>
2024-03-09 19:33:15 +0800

    update

--- version/GameData.h
+++ version/GameData.h
@@ -1,34 +1,33 @@
 #pragma once
 class GameData
 {
-	public:
-		bool	InfAmmo = false;
-		bool	InfAmmoLegit = false;
-		bool	InfGrenades = false;
-		bool	InfGrenadesLegit = false;
-		bool	InfSyringes = false;
-		bool	InfSyringesLegit = false;
-		bool	InfStratagems = false;
-		bool	InfMissionTime = false;
-		bool	InfStamina = false;
-		bool	InfHealth = false;
-		bool	NoReload = false;
-		bool	NoReload_v2 = false;
-		bool	MaxResources = false;
-		bool	Add5Resources = false;
-		bool	Recoil = false;
-		bool	InfBackpack = false;
-		bool	InfSpecWeapon = false;
-		bool	NoCannonOverheat = false;
-		bool	NoStasTurretOverHeat = false;
-		bool	JetpackNoCD = false;
-		bool	ShieldNoCD = false;
-		bool	InstantRailGun = false;
-		bool	ShowAllMapIcons = false;
-		bool	AllStratagems = false;
-		bool	AllEquipment = false;
-		bool	AllArmor = false;
-		bool	Speedhack = false;
+public:
+	bool	InfAmmo = true;
+	bool	InfAmmoLegit = true;
+	bool	InfGrenades = true;
+	bool	InfGrenadesLegit = false;
+	bool	InfSyringes = true;
+	bool	InfSyringesLegit = false;
+	bool	InfStratagems = true;
+	bool	InfMissionTime = false;
+	bool	InfStamina = true;
+	bool	InfHealth = true;
+	bool	NoReload = true;
+	bool	MaxResources = false;
+	bool	Add5Resources = false;
+	bool	Recoil = true;
+	bool	InfBackpack = true;
+	bool	InfSpecWeapon = true;
+	bool	NoCannonOverheat = true;
+	bool	NoStasTurretOverHeat = true;
+	bool	JetpackNoCD = true;
+	bool	ShieldNoCD = true;
+	bool	InstantRailGun = true;
+	bool	ShowAllMapIcons = false;
+	bool	AllStratagems = true;
+	bool	AllEquipment = true;
+	bool	AllArmor = true;
+	bool	Speedhack = false;
 
-		bool	OHK = false;
+	bool	OHK = false;
 };
